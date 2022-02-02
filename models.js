5796bc968f4807a2fd42712abe0b6e61283a9431
AG977 <93376305+AG977@users.noreply.github.com>
2022-02-02 23:06:19 +0100

    updated Birthday to DateOfBirth

--- models.js
+++ models.js
@@ -18,7 +18,7 @@ let userSchema = mongoose.Schema({
   Username: {type: String, required: true},
   Password: {type: String, required: true},
   Email: {type: String, required: true},
-  Birthday: Date,
+  DateOfBirth: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
