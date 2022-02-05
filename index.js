369dc337a80da0c401555c90c354890e7b043210
AG977 <93376305+AG977@users.noreply.github.com>
2022-02-05 22:50:10 +0100

    Included CORS in Index.js

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const methodOverride = require('method-override'),
   uuid = require("uuid");
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+const cors = require('cors');
+app.use(cors());
+
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
