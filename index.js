6f17a0a8b28deda880ba78b23f3100330ae16ccb
AG977 <93376305+AG977@users.noreply.github.com>
2022-02-27 13:29:11 +0100

    temporarily remove the authentication from movie

--- index.js
+++ index.js
@@ -48,9 +48,8 @@ app.get('/', (req, res) => {
 app.use(express.static('public'));
 
 // Gets the list of data about ALL Movies
-app.get('/movies',
+app.get('/movies', function (req, res) {
 //passport.authenticate('jwt', { session: false }),
-(req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
